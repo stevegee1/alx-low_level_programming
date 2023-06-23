20ead5a5eb29480f6d769e2a95533448fafbb038
stevegee1 <ogarstephen98@gmail.com>
2023-06-23 06:26:21 -0700

    updated 4-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-/* this is a comment */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 const char *msg = "\"Programming is like building a multilingual puzzle";
