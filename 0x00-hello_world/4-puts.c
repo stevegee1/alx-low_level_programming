a2aeb1a9c496700e868bd323638fcc1643dd6839
stevegee1 <ogarstephen98@gmail.com>
2023-06-23 03:43:24 -0700

    added 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-const char* message="\"Programming is like building a multilingual puzzle\n";
+const char* message="\"Programming is like building a multilingual puzzle";
 	puts(message);
 	return(0);
 }
