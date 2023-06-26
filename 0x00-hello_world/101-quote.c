457800f0c3d9e9f1989e16cd29fc4e5daf555cf9
stevegee1 <ogarstephen98@gmail.com>
2023-06-26 07:28:08 -0700

    Updated quote script

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-char message[] = "and that piece of art is useful\"
+char message[] = "and that piece of art is useful\" \
 - Dora Korpar, 2015-10-19\n";
 write(2, message, sizeof(message) - 1);
 return (1);
