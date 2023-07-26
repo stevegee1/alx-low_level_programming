8e045db7baa213afc58cf055e37490848964d36f
stevegee1 <ogarstephen98@gmail.com>
2023-07-26 16:20:14 -0700

    Added 6-abs file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - computes the abs of an integer
+ * @c: integer
+ * Return: Integer
+ */
+int _abs(int c)
+{
+	if (c >= 0)
+	{
+		return (c);
+	}
+return (c * -1);
+
+}
