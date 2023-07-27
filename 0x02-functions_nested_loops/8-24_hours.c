0438f2a2a6e80d7d53678668a403d2af13ade6aa
stevegee1 <ogarstephen98@gmail.com>
2023-07-26 17:41:21 -0700

    Added 8-24 file

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdio.h>
+
+/**
+ * jack_bauer - prints every minute of the day
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int x, y, w, z;
+
+	for (x = 0; x <= 2; x++)
+	{
+		for (y = 0; y <= 3; y++)
+		{
+			for (w = 0; w <= 5; w++)
+			{
+				for (z = 0; z <= 9; z++)
+				{
+					printf("%d%d:%d%d", (x), (y), (w), (z));
+					_putchar (32);
+				}
+			}
+		}
+	}
+}
+
