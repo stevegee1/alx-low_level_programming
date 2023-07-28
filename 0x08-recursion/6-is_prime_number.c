307bd7fd00b9c61cd068dd8aa7bae9dae3ae9765
stevegee1 <ogarstephen98@gmail.com>
2023-07-28 10:05:57 -0700

    Add 6-is_prime_number file and its 6-main.c file

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,36 @@
+/**
+ * _print_Number_helper - handles the logic of is_prime_num
+ * @n: Integer
+ * @x: integer
+ * Return: integer
+ */
+int _print_Number_helper(int n, int x)
+{
+	if (x > n / 2)
+	{
+		return (1);
+	}
+	if (n % x == 0)
+	{
+		return (0);
+	}
+	return (_print_Number_helper(n, x + 1));
+}
+/**
+ * _is_prime_number - returns 1 if prime number
+ * @n: integer
+ * Return: integer
+ */
+int is_prime_number(int n)
+{
+	int x = 1;
+	int z;
+
+	if (n <= 1)
+	{
+		return (0);
+	}
+	z = _print_Number_helper(n, x + 1);
+	return (z);
+}
+
